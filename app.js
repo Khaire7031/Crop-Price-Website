70898a172e2dca720a8d49fb92fd8753a8ee0b91
Pranav Khaire <pranavkhaire28@gmail.com>
2023-12-21 17:55:13 +0530

    new changes commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var currentDateElement = document.getElementById('currentDate');
+
+function updateDate() {
+    // Create a new Date object
+    var currentDate = new Date();
+
+    // Format the date (you can customize the format as needed)
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var formattedDate = currentDate.toLocaleDateString('en-US', options);
+
+    // Set the formatted date to the span element
+    currentDateElement.textContent = formattedDate;
+}
+
+// Update the date initially
+updateDate();
+
+// Update the date every 1 second (adjust the interval as needed)
+setInterval(updateDate, 1000);
